fe0ea872452c0fe432bb4d31dc5a54f4ceb18e71
snipe <snipe@snipe.net>
2015-08-25 18:04:29 -0700

    Nicer register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,27 +1,56 @@
-<form method="POST" action="/auth/register">
-    {!! csrf_field() !!}
+@extends('layouts/default')
 
-    <div class="col-md-6">
-        Name
-        <input type="text" name="name" value="{{ old('name') }}">
-    </div>
+{{-- Page title --}}
+@section('title')
+     Register ::
+@parent
+@stop
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
 
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
+{{-- Page content --}}
+@section('content')
 
-    <div class="col-md-6">
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
+<section class="container">
+    <div class="row">
 
-    <div>
-        <button type="submit">Register</button>
-    </div>
-</form>
+        <div class="col-md-6 col-sm-12 col-xs-12">
+
+        <form method="POST" action="/auth/register">
+            {!! csrf_field() !!}
+
+            <div class="form-group col-md-3">
+                <label for="name">Name</label>
+            </div>
+            <div class="form-group col-md-9">
+                <input type="text" name="name" value="{{ old('name') }}">
+            </div>
+
+            <div class="form-group col-md-3">
+                <label for="email">Email</label>
+            </div>
+            <div class="form-group col-md-9">
+                <input type="email" name="email" value="{{ old('email') }}">
+            </div>
+
+            <div class="form-group col-md-3">
+                <label for="password">Password</label>
+            </div>
+            <div class="form-group col-md-9">
+                <input type="password" name="password">
+            </div>
+
+            <div class="form-group col-md-3">
+                <label for="password_confirmation">Confirm Password</label>
+            </div>
+            <div class="form-group col-md-9">
+                <input type="password" name="password_confirmation">
+            </div>
+
+            <div class="form-group col-md-3">
+                <button type="submit" class="btn btn-primary">Register</button>
+            </div>
+        </form>
+
+    </div><!--end row-->
+</section>
+@stop
