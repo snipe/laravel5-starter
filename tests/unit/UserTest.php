e4b3df7882fd98c5f3f1455fc7bdcdbf3f061d71
snipe <snipe@snipe.net>
2015-09-18 02:21:45 -0700

    Updated test for first and last name

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -9,10 +9,17 @@ class UserTest extends \Codeception\TestCase\Test
     protected $tester;
     public function testRegister()
     {
+
         $email = 'johndoe@example.com';
-        $name = 'John Doe';
+        $first_name = 'John';
+        $last_name = 'Doe';
+        $slug = 'johndoe';
         $password = Hash::make('password');
-        User::register(['email' => $email, 'password' => $password, 'name' => $name]);
-        $this->tester->seeRecord('users', ['email' => $email, 'password' => $password, 'name' => $name]);
+
+        User::register(['email' => $email, 'password' => $password, 'first_name' => $first_name, 'last_name' => $last_name, 'slug' => $slug]);
+        $this->tester->seeRecord('users', ['email' => $email, 'password' => $password]);
+        
     }
+
+
 }
